d6dd2936010f50a3db40f27d015d730a96b307a1
jisuuuu <gjkl55@naver.com>
2025-09-02 16:04:01 +0900

    BOJ18108 - 1998년생인 내가 태국에서는 2541년생?!

---
+++ src/BOJ18108.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ18108 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int y = Integer.parseInt(br.readLine());
+        System.out.println(y - 543);
+    }
+}
