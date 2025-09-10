e297fadbe22a27a3f6b6f8968f9341232fbb7f97
jisuuuu <gjkl55@naver.com>
2025-09-10 14:25:52 +0900

    BOJ3052 - 나머지

---
+++ src/BOJ3052.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+
+public class BOJ3052 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < 10; i++) {
+            int value = Integer.parseInt(br.readLine());
+            set.add(value % 42);
+        }
+        System.out.println(set.size());
+        br.close();
+    }
+}
