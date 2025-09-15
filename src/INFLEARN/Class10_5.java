a37c384a5519a322ef38e70ddb2896e90d9cc7d7
jisuuuu <gjkl55@naver.com>
2025-09-15 19:50:06 +0900

    인프런 코딩테스트 강의 - 11) dynamic programming

---
+++ src/INFLEARN/Class10_5.java
@@ -0,0 +1,26 @@
+package INFLEARN;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Class10_5 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] coin = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int m = Integer.parseInt(br.readLine());
+        int[] dp = new int[m + 1];
+
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = coin[i]; j <= m; j++) {
+                dp[j] = Math.min(dp[j - coin[i]] + 1, dp[j]);
+            }
+        }
+        System.out.println(dp[m]);
+    }
+}
