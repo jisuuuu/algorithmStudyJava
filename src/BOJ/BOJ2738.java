13367ad1ca81056890ac738752b0eb4222d6b444
jisuuuu <gjkl55@naver.com>
2025-09-19 14:00:07 +0900

    BOJ2738 - 행렬 덧셈

---
+++ src/BOJ/BOJ2738.java
@@ -0,0 +1,34 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ2738 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[][] a = new int[n][m];
+        int[][] b = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            a[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        }
+
+        for (int i = 0; i < n; i++) {
+            b[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(a[i][j] + b[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
