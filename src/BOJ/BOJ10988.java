8d74be0f0d5331c51008e29d90be33a14bcb36dd
jisuuuu <gjkl55@naver.com>
2025-09-22 14:53:23 +0900

    BOJ10988 - 팰린드롬인지 확인하기

---
+++ src/BOJ/BOJ10988.java
@@ -0,0 +1,19 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ10988 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String word = br.readLine();
+        String reverse = new StringBuilder(word).reverse().toString();
+
+        if (word.equals(reverse)) {
+            System.out.println("1");
+        } else {
+            System.out.println("0");
+        }
+    }
+}
