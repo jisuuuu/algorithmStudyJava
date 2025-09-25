0d563aa98ca077758fc9c572e94e1977c1be30e8
jisuuuu <gjkl55@naver.com>
2025-09-25 13:32:35 +0900

    BOJ11005 - 진법 변환 2

---
+++ src/BOJ/BOJ11005.java
@@ -0,0 +1,27 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ11005 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        while (n > 0) {
+            if (n % b >= 10) {
+                sb.append((char) ((n % b) + 55));
+            } else {
+                sb.append(n % b);
+            }
+            n /= b;
+        }
+        System.out.println(sb.reverse());
+    }
+}
