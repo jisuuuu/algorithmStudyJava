00b490ea1eb72c0512082ffec197a8e830c74e8a
jisuuuu <gjkl55@naver.com>
2026-01-12 18:39:09 +0900

    BOJ2720 - 세탁소 사장 동혁

---
+++ src/BOJ/BOJ2720.java
@@ -0,0 +1,31 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ2720 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            int c = Integer.parseInt(br.readLine());
+
+            int quarter = c / 25;
+            c = c % 25;
+
+            int dime = c / 10;
+            c = c % 10;
+
+            int nickel = c / 5;
+            c = c % 5;
+
+            int penny = c;
+
+            System.out.printf("%d %d %d %d", quarter, dime, nickel, penny);
+            System.out.println();
+        }
+    }
+}
