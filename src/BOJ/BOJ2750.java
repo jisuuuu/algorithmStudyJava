7da62ddbf4737dd5ebb585c27eafef7a80fe1679
jisuuuu <gjkl55@naver.com>
2026-02-09 15:48:52 +0900

    BOJ2750 - 수 정렬하기

---
+++ src/BOJ/BOJ2750.java
@@ -0,0 +1,25 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class BOJ2750 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            list.add(Integer.parseInt(br.readLine()));
+        }
+        Collections.sort(list);
+
+        for (int a : list) {
+            System.out.println(a);
+        }
+    }
+}
